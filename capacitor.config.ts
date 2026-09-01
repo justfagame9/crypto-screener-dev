893f21057769c2aa2b7e44bbf82ac46b1c26ec49
justfagame9 <justfagame9@gmail.com>
2026-09-02 00:55:02 +0530

    updated name to TradeSphere

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cryptoscreener.ichimoku',
-  appName: 'Crypto Screener',
+  appName: 'TradeSphere',
   webDir: 'www'
 };
 
